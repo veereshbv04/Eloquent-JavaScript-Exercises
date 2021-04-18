c6f9953dfdd41b4159957774d071174e15e460bd
Veeresh B V <veereshbv04@gmail.com>
2021-04-18 19:30:11 +0530

    Create sum_of_range_of_numbers.js

---
+++ sum_of_range_of_numbers.js
@@ -0,0 +1,15 @@
+function sum(...r){
+  let total = 0,count=1;
+  while(count<=10){
+    total+=count;
+    count++
+  }
+  return total;
+}
+function range(n,m){
+
+}
+
+
+
+console.log(sum(range(1,10)))
